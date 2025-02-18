959f1e4ee920ebd109e3e326d573a0f44573b525
FelipeFMA <felipefmavelar@gmail.com>
2025-02-18 06:23:16 -0300

    added the code

---
+++ casino.py
@@ -0,0 +1,34 @@
+import random
+
+x = random.randint(1, 3)
+y = random.randint(1, 3)
+z = random.randint(1, 3)
+
+if x == 1:
+    x = "🍒"
+if y == 1:
+    y = "🍒"
+if z == 1:
+    z = "🍒"
+
+if x == 2:
+    x = "🍉"
+if y == 2:
+    y = "🍉"
+if z == 2:
+    z = "🍉"
+
+if x == 3:
+    x = "🍌"
+if y == 3:
+    y = "🍌"
+if z == 3:
+    z = "🍌"
+
+
+print("\n", x, y, z, "\n")
+
+if x == y == z:
+    print("Você Ganhou!\n")
+else:
+    print("Não foi dessa vez\n")
